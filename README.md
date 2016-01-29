c5fcd0bc82651d44665e2b62ebdea33d4dc29d98
Limech <limech@gmail.com>
2016-01-28 21:13:27 -0500

    Add code and updated readme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # brush-ada
-Ada syntax highlighting for SyntaxHighlighter.  Can be used with Confluence Code Block Macro.
+Ada syntax highlighting for [SyntaxHighlighter](http://alexgorbatchev.com/SyntaxHighlighter/ "SyntaxHighlighter").  
+Can be used with [Confluence's Code Block Macro](https://confluence.atlassian.com/doc/code-block-macro-139390.html "Confluence Code Block Macro").
---
+++ brush-ada.js
@@ -0,0 +1,58 @@
+
+SyntaxHighlighter.brushes.Custom = function()
+{
+    
+  var datatypes = 'Integer Float Duration Character String Boolean';
+
+  var keywords = 'abort abs abstract accept access aliased all and array at begin body case' +
+           'constant declare delay delta digits do else elsif end entry exception exit for' +
+           'function generic goto if in interface is limited loop mod new not null of or others' +
+           'out overriding package pragma private procedure protected raise range record rem renames' +
+           'requeue return reverse select separate some subtype synchronized tagged task terminate' +
+           'then type until use when while with xor';
+
+  var functions = 'begin continue switch break result finally raise while then case else goto' +
+           'and for try xor not end as do if or in is to downto';
+
+  this.regexList = [
+    {
+      regex: SyntaxHighlighter.regexLib.singleLineCComments,
+      css: 'comments'
+    },
+    {
+      regex: SyntaxHighlighter.regexLib.multiLineCComments,
+      css: 'comments'
+    },
+    {
+      regex: SyntaxHighlighter.regexLib.doubleQuotedString,
+      css: 'string'
+    },
+    {
+      regex: SyntaxHighlighter.regexLib.singleQuotedString,
+      css: 'string'
+    },
+    {
+      regex: /^ *#.*/gm,
+      css: 'preprocessor'
+    },
+    {
+      regex: /--.*$/gm,
+      css: 'comments'
+    },
+    {
+      regex: new RegExp(this.getKeywords(datatypes), 'gm'),
+      css: 'color1 bold'
+    },
+    {
+      regex: new RegExp(this.getKeywords(functions), 'gm'),
+      css: 'functions bold'
+    },
+    {
+      regex: new RegExp(this.getKeywords(keywords), 'gm'),
+      css: 'keyword bold'
+    }
+   ];
+};
+
+SyntaxHighlighter.brushes.Custom.prototype  = new SyntaxHighlighter.Highlighter();
+SyntaxHighlighter.brushes.Custom.aliases  = ['ada'];
